f95f2232a80a86f1e1ca52cbcd28d21a4fac49a9
Bill Hunt <bill.hunt@willowtreeapps.com>
2013-10-11 17:12:27 -0400

    Adding content specific to the www domain.

--- htdocs/api-key.php
+++ htdocs/api-key.php
@@ -66,7 +66,7 @@ $api = new API();
 $sidebar = '<h1>Nota Bene</h1>
 	<section>
 		<p>'.SITE_TITLE.' is not your database. Cache accordingly.</p>
-		
+
 		<p>Consider whether <a href="/downloads/">a bulk download</a> might be more appropriate
 		for your purposes.</p>
 	</section>';
@@ -81,7 +81,7 @@ if (isset($_POST['form_data']))
 	 * Pass the submitted form data to the API class, as an object rather than as an array.
 	 */
 	$api->form = (object) $_POST['form_data'];
-	
+
 	/*
 	 * If this form hasn't been completed properly, display the errors and re-display the form.
 	 */
@@ -90,13 +90,13 @@ if (isset($_POST['form_data']))
 		$body .= '<p class="error">Error: '.$api->form_errors.'</p>';
 		$body .= $api->display_form();
 	}
-	
+
 	/*
 	 * But if the form has been filled out correctly, then proceed with the registration process.
 	 */
 	else
 	{
-		
+
 		/*
 		 * Register this key.
 		 */
@@ -108,11 +108,11 @@ if (isset($_POST['form_data']))
 		{
 			$body = '<p class="error">Error: ' . $e->getMessage() . '</p>';
 		}
-		
+
 		$body .= '<p>You have been sent an e-mail to verify your e-mail address. Please click the
 					link in that e-mail to activate your API key.</p>';
 	}
-	
+
 }
 
 /*
@@ -133,10 +133,10 @@ elseif (isset($_GET['secret']))
 		$api->secret = $_GET['secret'];
 		$api->activate_key();
 		$body .= '<h2>API Key Activated</h2>
-				
+
 				<p>Your API key has been activated. You may now make requests from the API. Your
 				key is:</p>
-				
+
 				<p><code>'.$api->key.'</code></p>';
 	}
 }
@@ -146,7 +146,57 @@ elseif (isset($_GET['secret']))
  */
 else
 {
-	$body = $api->display_form();
+$template->field->page_title = 'API &amp; Downloads';
+
+	$body = '
+	<p>
+		To use the San Francisco Decoded API, you will need to register for a key below.
+		You may want to <a href="https://github.com/statedecoded/statedecoded/wiki/API-Documentation">read
+		the documentation</a> first.
+	</p>
+	<p>
+		Note that you will need to use the proper full url
+		for each section of the code that you want to interface with - each section has a
+		separate subdomain.  E.g., for the Business codes, you should be using the
+		<code>business.sanfranciscocode.org</code> domain.
+	</p>
+	<p>
+		Alternately, you may download the bulk data for any section.  A list of those are
+		below.
+	</p>
+	<h3>Bulk Data Downloads</h3>
+	<ul class="bulk-data-links">
+		' . /*<li><a href="http://administrative.sanfranciscocode.org/downloads/">Administrative</a></li>*/ '
+		' . /*<!--li><a href="http://building.sanfranciscocode.org/downloads/">Building</a></li>*/ '
+		<li><a href="http://business.sanfranciscocode.org/downloads/">Business</a></li>
+		<li><a href="http://campaign.sanfranciscocode.org/downloads/">Campaign</a></li>
+		<li><a href="http://charter.sanfranciscocode.org/downloads/">Charter</a></li>
+		<li><a href="http://elections.sanfranciscocode.org/downloads/">Elections</a></li>
+		<li><a href="http://electrical.sanfranciscocode.org/downloads/">Electrical</a></li>
+	</ul>
+	<ul class="bulk-data-links">
+		<li><a href="http://environment.sanfranciscocode.org/downloads/">Environment</a></li>
+		<li><a href="http://fire.sanfranciscocode.org/downloads/">Fire</a></li>
+		<li><a href="http://health.sanfranciscocode.org/downloads/">Health</a></li>
+		<li><a href="http://housing.sanfranciscocode.org/downloads/">Housing</a></li>
+	</ul>
+	<ul class="bulk-data-links">
+		<li><a href="http://mechanical.sanfranciscocode.org/downloads/">Mechanical</a></li>
+		<li><a href="http://park.sanfranciscocode.org/downloads/">Park</a></li>
+		<li><a href="http://planning.sanfranciscocode.org/downloads/">Planning</a></li>
+		<li><a href="http://plumbing.sanfranciscocode.org/downloads/">Plumbing</a></li>
+	</ul>
+	<ul class="bulk-data-links">
+		<li><a href="http://port.sanfranciscocode.org/downloads/">Port</a></li>
+		<li><a href="http://public-works.sanfranciscocode.org/downloads/">Public Works</a></li>
+		<li><a href="http://subdivision.sanfranciscocode.org/downloads/">Subdivision</a></li>
+		<li><a href="http://transportation.sanfranciscocode.org/downloads/">Transportation</a></li>
+	</ul>
+	';
+
+	$body .= '<section class="api-register"><h1>Register for an API Key</h1>';
+	$body .= $api->display_form();
+	$body .= '</section>';
 }
 
 /*
--- htdocs/index.php
+++ htdocs/index.php
@@ -41,30 +41,58 @@ $body = '';
 /*
  * Initialize the sidebar variable.
  */
-$sidebar = '';
+$sidebar = '
+	<section>
+	<h1>Welcome</h1>
+	<p>
+		SanFranciscoCode.org provides the San Francisco City laws, rules and
+		regulations on one friendly website. No copyright restrictions, a
+		modern API and all of the niceties of modern website design. It’s like
+		the expensive software lawyers use, but free and wonderful.
+	</p>
+	<p>
+		This is a public beta test of <a href="http:///www.sanfranciscocode.org">SanFranciscoCode.org</a>, which is to say that
+		everything is under development. Things may be funny looking, broken, and
+		generally under development right now.
+	</p>
+	</section>
+	<section>
+		<h1>Stay Updated</h1>
+		<p>
+			<a href="http://eepurl.com/FUc0b">Click here to join our mailing list</a>
+		</p>
+		<p>
+			Want to open your city or state? <a href="mailto:sayhello@opengovfoundation.org?Subject=Help%20Open%20My%20City%20or%20State">Drop us a line!</a>
+		</p>
+	</section>';
 
 
-/*
- * Get an object containing a listing of the fundamental units of the code.
- */
-$struct = new Structure();
-$structures = $struct->list_children();
 
-$body .= '
-	<article>
-	<h1>'.ucwords($structures->{0}->label).'s of the '.LAWS_NAME.'</h1>
-	<p>These are the fundamental units of the '.LAWS_NAME.'.</p>';
-if ( !empty($structures) )
-{
-	$body .= '<dl class="level-1">';
-	foreach ($structures as $structure)
-	{
-		$body .= '	<dt><a href="'.$structure->url.'">'.$structure->identifier.'</a></dt>
-					<dd><a href="'.$structure->url.'">'.$structure->name.'</a></dd>';
-	}
-	$body .= '</dl>';
-}
-$body .= '</article>';
+$body .= '<article>
+	<h1>Sections of the '.LAWS_NAME.'</h1>
+	<p>These are the sections of the '.LAWS_NAME.'.</p>
+	<ul class="level-1">
+		' . /*<li><a href="http://administrative.sanfranciscocode.org/">Administrative</a></li>*/ '
+		' . /*<!--li><a href="http://building.sanfranciscocode.org/">Building</a></li>*/ '
+		<li><a href="http://business.sanfranciscocode.org/">Business</a></li>
+		<li><a href="http://campaign.sanfranciscocode.org/">Campaign</a></li>
+		<li><a href="http://charter.sanfranciscocode.org/">Charter</a></li>
+		<li><a href="http://elections.sanfranciscocode.org/">Elections</a></li>
+		<li><a href="http://electrical.sanfranciscocode.org/">Electrical</a></li>
+		<li><a href="http://environment.sanfranciscocode.org/">Environment</a></li>
+		<li><a href="http://fire.sanfranciscocode.org/">Fire</a></li>
+		<li><a href="http://health.sanfranciscocode.org/">Health</a></li>
+		<li><a href="http://housing.sanfranciscocode.org/">Housing</a></li>
+		<li><a href="http://mechanical.sanfranciscocode.org/">Mechanical</a></li>
+		<li><a href="http://park.sanfranciscocode.org/">Park</a></li>
+		<li><a href="http://planning.sanfranciscocode.org/">Planning</a></li>
+		<li><a href="http://plumbing.sanfranciscocode.org/">Plumbing</a></li>
+		<li><a href="http://port.sanfranciscocode.org/">Port</a></li>
+		<li><a href="http://public-works.sanfranciscocode.org/">Public Works</a></li>
+		<li><a href="http://subdivision.sanfranciscocode.org/">Subdivision</a></li>
+		<li><a href="http://transportation.sanfranciscocode.org/">Transportation</a></li>
+	</ul>
+</article>';
 
 /*
  * Put the shorthand $body variable into its proper place.
