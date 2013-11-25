a48643bf107efa22a50bac1c7358b770ddb342f8
Bill Hunt <bill.hunt@willowtreeapps.com>
2013-11-25 11:19:52 -0500

    Tweaking home page for browse link.

--- htdocs/home.php
+++ htdocs/home.php
@@ -39,36 +39,39 @@ $body = '<!-- UNCOMMENT TO DISPLAY AN INTRODUCTORY VIDEO HERE
 	<section class="homepage" role="main">
 		<div class="nest">
 			<div>
-			<section class="feature-content" id="sections-feature">
+			<section class="feature-content">
 				<hgroup>
 					<h1>Discover the ' . LAWS_NAME . '</h1>
 					<h2>The laws of ' . PLACE_NAME . ', for non-lawyers.</h2>
 				</hgroup>
 
-				<p>These are the sections of the '.LAWS_NAME.'.</p>
-				<ul class="code-sections-list">
-					<li><a href="http://administrative.'.BASE_SITE_DOMAIN.'/">Administrative</a></li>
-					' . /*<!--li><a href="http://building.'.BASE_SITE_DOMAIN.'/">Building</a></li>*/ '
-					<li><a href="http://business.'.BASE_SITE_DOMAIN.'/">Business</a></li>
-					<li><a href="http://campaign.'.BASE_SITE_DOMAIN.'/">Campaign</a></li>
-					<li><a href="http://charter.'.BASE_SITE_DOMAIN.'/">Charter</a></li>
-					<li><a href="http://elections.'.BASE_SITE_DOMAIN.'/">Elections</a></li>
-					<li><a href="http://electrical.'.BASE_SITE_DOMAIN.'/">Electrical</a></li>
-					<li><a href="http://environment.'.BASE_SITE_DOMAIN.'/">Environment</a></li>
-					<li><a href="http://fire.'.BASE_SITE_DOMAIN.'/">Fire</a></li>
-					<li><a href="http://health.'.BASE_SITE_DOMAIN.'/">Health</a></li>
-				</ul>
-				<ul>
-					<li><a href="http://housing.'.BASE_SITE_DOMAIN.'/">Housing</a></li>
-					<li><a href="http://mechanical.'.BASE_SITE_DOMAIN.'/">Mechanical</a></li>
-					<li><a href="http://park.'.BASE_SITE_DOMAIN.'/">Park</a></li>
-					<li><a href="http://planning.'.BASE_SITE_DOMAIN.'/">Planning</a></li>
-					<li><a href="http://plumbing.'.BASE_SITE_DOMAIN.'/">Plumbing</a></li>
-					<li><a href="http://port.'.BASE_SITE_DOMAIN.'/">Port</a></li>
-					<li><a href="http://public-works.'.BASE_SITE_DOMAIN.'/">Public Works</a></li>
-					<li><a href="http://subdivision.'.BASE_SITE_DOMAIN.'/">Subdivision</a></li>
-					<li><a href="http://transportation.'.BASE_SITE_DOMAIN.'/">Transportation</a></li>
-				</ul>
+				<section id="sections-feature">
+					<p>These are the sections of the '.LAWS_NAME.'.</p>
+
+					<ul class="code-sections-list">
+						<li><a href="http://administrative.'.BASE_SITE_DOMAIN.'/">Administrative</a></li>
+						' . /*<!--li><a href="http://building.'.BASE_SITE_DOMAIN.'/">Building</a></li>*/ '
+						<li><a href="http://business.'.BASE_SITE_DOMAIN.'/">Business</a></li>
+						<li><a href="http://campaign.'.BASE_SITE_DOMAIN.'/">Campaign</a></li>
+						<li><a href="http://charter.'.BASE_SITE_DOMAIN.'/">Charter</a></li>
+						<li><a href="http://elections.'.BASE_SITE_DOMAIN.'/">Elections</a></li>
+						<li><a href="http://electrical.'.BASE_SITE_DOMAIN.'/">Electrical</a></li>
+						<li><a href="http://environment.'.BASE_SITE_DOMAIN.'/">Environment</a></li>
+						<li><a href="http://fire.'.BASE_SITE_DOMAIN.'/">Fire</a></li>
+						<li><a href="http://health.'.BASE_SITE_DOMAIN.'/">Health</a></li>
+					</ul>
+					<ul>
+						<li><a href="http://housing.'.BASE_SITE_DOMAIN.'/">Housing</a></li>
+						<li><a href="http://mechanical.'.BASE_SITE_DOMAIN.'/">Mechanical</a></li>
+						<li><a href="http://park.'.BASE_SITE_DOMAIN.'/">Park</a></li>
+						<li><a href="http://planning.'.BASE_SITE_DOMAIN.'/">Planning</a></li>
+						<li><a href="http://plumbing.'.BASE_SITE_DOMAIN.'/">Plumbing</a></li>
+						<li><a href="http://port.'.BASE_SITE_DOMAIN.'/">Port</a></li>
+						<li><a href="http://public-works.'.BASE_SITE_DOMAIN.'/">Public Works</a></li>
+						<li><a href="http://subdivision.'.BASE_SITE_DOMAIN.'/">Subdivision</a></li>
+						<li><a href="http://transportation.'.BASE_SITE_DOMAIN.'/">Transportation</a></li>
+					</ul>
+				</section>
 
 				<p>' . SITE_TITLE . ' provides the ' . LAWS_NAME . ' on one friendly website. Inline
 				definitions, cross-references, bulk downloads, a modern API, and all of the niceties
