56c927d9e5864fb9a255e67c1e85009cec9f4dff
Bill Hunt <bill.hunt@willowtreeapps.com>
2013-11-26 21:47:25 -0500

    Make deploy script clean up after itself.

--- deploy/config/deploy.rb
+++ deploy/config/deploy.rb
@@ -34,6 +34,10 @@ set :use_sudo, false
 
 set(:deploy_to) { "/var/www/releases/#{application}/#{site}/#{stage}" }
 
+# Delete all of the old releases please!
+set :keep_releases, 3
+after "deploy:update", "deploy:cleanup"
+
 # Don't do the normal timestamp update, as it uses Rails paths.
 set :normalize_asset_timestamps, false
 
